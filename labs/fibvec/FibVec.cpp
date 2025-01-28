abd9a675dfed5a53bd2d67dbb3dcdab8681b6bc6
John Sylvester <johnsylvester@csilvm-17.cs.ucsb.edu>
2025-01-28 02:14:25 -0800

    First try

--- labs/fibvec/FibVec.cpp
+++ labs/fibvec/FibVec.cpp
@@ -1,7 +1,127 @@
 #include "FibVec.h"
-
+#include <iostream>
 // This provides exception types:
 #include <stdexcept>
 
-
 // FibVec Function Implementations
+size_t FibVec::fibNewSize(int element){
+	if(element >= 2){
+		return fibNewSize(element - 1) + fibNewSize(element -2);
+	}
+	return element;
+}
+
+size_t FibVec::capacity() const{
+	return cap;
+}
+
+size_t FibVec::count() const{
+        return cou;
+}
+
+void FibVec::reSize(int* &array, size_t size){
+	int* array2 = new int[size];
+	for(size_t i = 0; i < cou; i++){
+		array2[i] = array[i];	
+	}
+
+	delete [] array;
+
+	array = array2;
+
+	cap = size;
+	std::cout << "Resized to " << cap << std::endl;
+}
+
+void FibVec::push(int value){
+	if(cou == cap){
+		ele++;
+		reSize(array, fibNewSize(ele));
+	}
+
+	array[int(cou)] = value;
+	cou++;
+}
+
+int FibVec::pop(){
+	try{
+		if(cou <= 0){
+		throw (std::underflow_error("Underflow"));
+		}
+		int val = array[int(cou-1)];
+		cou--;
+		if(cou < fibNewSize(ele-2)){
+                	ele--;
+                	reSize(array, fibNewSize(ele));
+        	}
+		return val;
+	}
+	catch(const std::underflow_error& e){
+		std::cout << "Underflow\n";
+		return -1;
+		//delete [] array;
+		//std::abort();
+	}
+}
+
+int FibVec::lookup(size_t index) const{
+        try
+	{
+	if(index > cou-1){
+                throw (std::out_of_range("Range"));
+                }
+	return array[index];
+	}
+	catch(const std::out_of_range& e){
+                std::cout << "Range\n";
+		delete [] array;
+		exit(-1);
+        }
+}
+
+FibVec::FibVec(){
+	size_t current = fibNewSize(2);
+	array = new int[current];
+	cap = current;
+	ele = 2;
+	cou = 0;
+}
+FibVec::~FibVec(){
+	delete [] array;
+}
+
+void FibVec::insert(int value, size_t index){
+        if(cou == cap){
+                ele++;
+                reSize(array, fibNewSize(ele));
+        }
+	for(unsigned int i = cou; i >= index; i--){
+		array[i+1] = array[i];
+	}
+	array[index] = value;
+
+        cou++;
+}
+
+int FibVec::remove(size_t index){
+        try{
+                if(cou <= 0){
+                throw (std::underflow_error("Underflow"));
+                }
+		int val = array[int(cou)];
+                cou--;
+                if(cou < fibNewSize(ele-2)){
+                        ele--;
+                        reSize(array, fibNewSize(ele));
+                }
+		for(unsigned int i = index; i <= cou; i++){
+                array[i] = array[i+1];
+        }
+		return val;
+        }
+        catch(const std::underflow_error& e){
+                std::cout << "Underflow\n";
+		//delete [] array;
+		return -1;
+        }
+}
--- labs/fibvec/FibVec.h
+++ labs/fibvec/FibVec.h
@@ -6,14 +6,27 @@
 
 class FibVec {
   // Member Variables
+	size_t cap;
+	size_t cou;
+	size_t ele;
+	int* array;
 
   // Helper Functions
-
+	size_t fibNewSize(int element);
+	void reSize(int*& array, size_t size);
 public:
   // Constructor and Destructor
-
+	FibVec();
+	~FibVec();
   // Member Functions
-
+	size_t capacity() const;
+	size_t count() const;
+	void insert(int value, size_t index);
+	int lookup(size_t index) const;
+	int pop();
+	void push(int value);
+	int remove(size_t index);
+		
 };
 
 #endif
